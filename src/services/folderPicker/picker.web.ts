500dfee1bc383db557d771d59f2df2c0a781a55f
Subrata Mondal <subratamondal@tutanota.com>
2026-09-20 19:52:14 +0530

    feat(web): implement folder picker for web

--- src/services/folderPicker/picker.web.ts
+++ src/services/folderPicker/picker.web.ts
@@ -1,3 +1,35 @@
-export default function folderPicker() {
-  console.log("Folder picker called on web");
+import { Video } from "@/types/video.type";
+
+export default async function folderPicker(): Promise<Video[]> {
+  try {
+    const showDirectoryPicker = window.showDirectoryPicker;
+
+    if (!showDirectoryPicker) {
+      throw new Error("Directory picker is not supported in this environment", {
+        cause: "CUSTOM",
+      });
+    }
+
+    const dirHandler = await showDirectoryPicker();
+    const videos: Video[] = [];
+
+    for await (const entry of dirHandler.values()) {
+      if (entry.kind == "directory") continue;
+
+      const file = await entry.getFile();
+      videos.push({ name: file.name, size: file.size });
+    }
+
+    return videos;
+  } catch (error) {
+    if (error instanceof DOMException && error.name === "AbortError") {
+      throw new Error("Directory selection cancelled", { cause: error });
+    }
+
+    if ((error as Error).cause === "CUSTOM") {
+      throw error;
+    } else {
+      throw new Error("Failed to read directory", { cause: error });
+    }
+  }
 }
---
+++ src/types/video.type.ts
@@ -0,0 +1,5 @@
+export interface Video {
+    uri?: string
+    name: string
+    size: number
+}
