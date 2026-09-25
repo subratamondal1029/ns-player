aa7d7a77fd8b4084f0216a3e744b281729982f6d
Subrata Mondal <subratamondal@tutanota.com>
2026-09-25 21:17:00 +0530

    feat(tailwind): add custom font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
+    fontSize: {
+      xs: ["0.875rem", { lineHeight: "1.25rem" }],   // 14px
+      sm: ["1rem", { lineHeight: "1.5rem" }],        // 16px
+      base: ["1.125rem", { lineHeight: "1.75rem" }], // 18px
+      lg: ["1.25rem", { lineHeight: "1.75rem" }],    // 20px
+      xl: ["1.5rem", { lineHeight: "2rem" }],        // 24px
+      "2xl": ["1.875rem", { lineHeight: "2.25rem" }], // 30px
+      "3xl": ["2.25rem", { lineHeight: "2.5rem" }],  // 36px
+      "4xl": ["2.75rem", { lineHeight: "1" }],       // 44px
+      "5xl": ["3.5rem", { lineHeight: "1" }],        // 56px
+    },
   },
   plugins: [],
 };
