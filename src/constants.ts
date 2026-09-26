02089d56edf85be7b79350c98597af0b902ebd6d
Subrata Mondal <subratamondal@tutanota.com>
2026-09-26 20:14:04 +0530

    fix(env): remove trailing slash from server url

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const DB_NAME = "ns-player";
 export const DIR_STORE_NAME = "directories";
 export const DIR_STORE_KEY = "dir_main";
 
-export const SERVER_URL = `${process.env.EXPO_PUBLIC_SERVER_URL ?? ""}/`; // "/" for production
+export const SERVER_URL = process.env.EXPO_PUBLIC_SERVER_URL ?? ""; // current host for production
 
 // AsyncStorage (Android)
 export const ASYNC_STORAGE_DIR_KEY = "@ns-player/dir_main";
